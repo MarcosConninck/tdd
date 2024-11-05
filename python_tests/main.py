f41724867a345e6a2259d7f9d9f28091acf7df60
marcos <git.conninck@gmail.com>
2024-11-05 11:58:50 -0300

    update calculadora/main -> doctests

--- python_tests/main.py
+++ python_tests/main.py
@@ -1,6 +1,10 @@
 from calculadora import soma
 
 # print(soma(2, 3))
-# print(soma(4, 3))
-print(soma(1, 3))
-print(soma(1, 3))
+# print(soma(4.3, 3.1))
+# print(soma(1, 3))
+
+try:
+    print(soma(1, "10"))
+except AssertionError as e:
+    print(f'conta inválida: {e}')
